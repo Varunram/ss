530a81c4dc82f5e32f2b712508ba0e21f3e4c392
Varunram <vrg2009@ymail.com>
2019-03-12 17:16:30 +0530

    add decrypt benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -15,10 +15,24 @@ func BenchmarkTest(b *testing.B) {
   }
 }
 
-func BenchmarkAes(b *testing.B) {
+func BenchmarkAesEncrypt(b *testing.B) {
   data := []byte("shoulder artefact abstract position deny example shoulder myth orchard wolf jewel coconut tourist wrong cram")
   for i := 0 ; i < b.N ; i ++ {
     _, err := Encrypt(data, "password")
+    if err != nil {
+      b.Fatal(err)
+    }
+  }
+}
+
+func BenchmarkAesDecrypt(b *testing.B) {
+  data := []byte("shoulder artefact abstract position deny example shoulder myth orchard wolf jewel coconut tourist wrong cram")
+  x, err := Encrypt(data, "password")
+  if err != nil {
+    b.Fatal(err)
+  }
+  for i := 0 ; i < b.N ; i ++ {
+    _, err := Decrypt(x, "password")
     if err != nil {
       b.Fatal(err)
     }
